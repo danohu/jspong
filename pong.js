12cb470bde7c28d5d2c6c999bb255a7eccd74ae2
Dan O'Huiginn <daniel@ohuiginn.net>
2014-11-06 16:08:18 +0100

    Game loop and ball motion
    closes #3, #4

--- pong.js
+++ pong.js
@@ -1,13 +1,25 @@
+
+
+// XXX there is NO WAY this needs to be here, but I am shit at JS
+mainloop = function(){
+   game.mainloop();
+        };
+
+
 $(document).ready(function(){
     console.log('hello world');
     game = new Game(true);
     game.setup();
     game.run();
+
+
+
 })
 
 var GameObject = Class.extend({
     posx: null,
     posy: null,
+    update: function(){console.log('updating');},
     draw: function(){console.log('drawing');},
     init: function(game){
 	this.game = game},
@@ -19,10 +31,17 @@ var Paddle = GameObject.extend({
     });
 
 var Ball = GameObject.extend({
-    posx: 50,
-    posy: 50,
+    posx: 350,
+    posy: 250,
+    velocity_x: 2,
+    velocity_y: 1,
     radius: 5,
     
+    update: function(){
+	this.posx += this.velocity_x;
+	this.posy += this.velocity_y;
+	// XXX: here goes bounce-handling code
+	},
     draw: function(){
 	console.log(this.posx, this.posy, this.radius);
 	this.game.context.beginPath();
@@ -31,7 +50,6 @@ var Ball = GameObject.extend({
 	this.game.context.strokeStyle = 'green';
 	this.game.context.stroke();
 	this.game.context.fill();
-	console.log('ball goes here');
 	}
 });
 
@@ -40,28 +58,49 @@ var Game = Class.extend({
     setup: function(){
 	this.board = $('#board_outer');
 	this.canvas = $('<canvas>', {
-	    'id': 'board_inner',
-	    'height': "500",
-	    'width': "700",});
+	    'id': 'board_inner'});
+	// XXX not sure why specifying these on creation turns them into just style attributes
+	this.canvas.attr('width', '700');
+	this.canvas.attr('height', '500');
 	this.board.append(this.canvas);
 	this.context = $('#board_inner')[0].getContext("2d");
 	this.leftPaddle = new Paddle(this);
 	this.rightPaddle = new Paddle(this);
 	this.ball = new Ball(this);
 	this.gameObjects = [this.ball, this.leftPaddle, this.rightPaddle];
+	this.refreshRate = 1000 / 60;
       console.log(this.board.html());},
     draw: function(){
-	this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
+	this.context.clearRect(0,0,this.canvas.width(), this.canvas.height());
 	for(item in this.gameObjects){
 	    this.gameObjects[item].draw();
 	    }
 	},
-    run: function(){
-	while(this.active){
-	    this.active = false;
-	    this.draw();
-	    console.log('gameloop');
+    update: function(){
+	for(item in this.gameObjects){
+	    this.gameObjects[item].update();
 	    }
-	console.log('game over');
 	},
+
+    mainloop: function(){
+	    game = this; //XXX debug
+	    game.active = false;
+	    game.update();
+	    game.draw();	    
+	    // just for testing
+	    /*game._numloops = game._numloops || 0
+	    game._numloops += 1;
+	    console.log('gameloop' + game._numloops);
+	    if(game._numloops > 10) game.stoprunning();*/
+    },
+    run: function(){
+	var foo = function(){console.log('fauxloop');}
+	this._loopid = setInterval(mainloop, this.refreshRate);
+	
+	},
+    stoprunning: function(){
+	clearInterval(this._loopid);
+	}
 });
+
+
